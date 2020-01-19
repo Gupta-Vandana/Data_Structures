90192c12388acd23345ea48ee4126463f14c20c3
vandana gupta <vandanagupta196@gmail.com>
2020-01-19 22:47:00 +0530

    Stack

--- Stack/src/Client.java
+++ Stack/src/Client.java
@@ -4,20 +4,20 @@ public class Client {
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
-		Scanner scn=new Scanner(System.in);
-		
-		String str=scn.next();
-		Stack s= new Stack<>(str.length());
+		Scanner scn = new Scanner(System.in);
+
+		String str = scn.next();
+		Stack s = new Stack<>(str.length());
 	}
+
 	@SuppressWarnings("unchecked")
-	public static void DuplicateParen(String str,Stack s){
-	
-		for(int i=0;i<=str.length();i++){
-			char ch=str.charAt(i);
+	public static void DuplicateParen(String str, Stack s) {
+
+		for (int i = 0; i <= str.length(); i++) {
+			char ch = str.charAt(i);
 			s.Push(ch);
 		}
-		
-		
+
 	}
 
 }
