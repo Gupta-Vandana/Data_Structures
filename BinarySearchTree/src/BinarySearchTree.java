79e048d83ac674076a44c5a6e56a30d58ae3ae51
vandana gupta <vandanagupta196@gmail.com>
2020-05-25 10:58:50 +0530

    NEVER GIVE UP!

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BinarySearchTree {
 	Node root;
@@ -79,8 +81,6 @@ public class BinarySearchTree {
 		}
 	}
 
-	// Checking and Searching :
-	// 1.Find the node with minimum value in a Binary Search Tree
 	public int Minimum() {
 		return Minimum(root);
 	}
@@ -93,34 +93,15 @@ public class BinarySearchTree {
 
 	}
 
-	// 2.Check if the given array can represent Level Order Traversal of Bst
-	// 3.Check if a given array can represent Preorder Traversal of Binary
-	// Search Tree
-	// 4.Lowest Common Ancestor in a Binary Search Tree.
-	public int LCA(int val1, int val2) {
-		Node n = LCA(root, val1, val2);
-		if (n == null) {
-			return 0;
-
-		} else {
-			return n.data;
-		}
+	public int Maximum() {
+		return Maximum(root);
 	}
 
-	public Node LCA(Node node, int val1, int val2) {
-		if (node == null) {
-			return null;
+	public int Maximum(Node node) {
+		if (node.right == null) {
+			return node.data;
 		}
-		if (node.data == val1 || node.data == val2) {
-			return node;
-		}
-
-		if (node.data > val1 && node.data > val2)
-			return LCA(node.left, val1, val2);
-
-		if (node.data < val1 && node.data < val2)
-			return LCA(node.right, val1, val2);
-		return node;
+		return Maximum(node.right);
 	}
 
 	public int Size(Node node) {
@@ -143,89 +124,73 @@ public class BinarySearchTree {
 		return Math.max(l, r) + 1;
 	}
 
-	public int Maximum() {
-		return Maximum(root);
-	}
-
-	public int Maximum(Node node) {
-		if (node.right == null) {
-			return node.data;
-		}
-		return Maximum(node.right);
-	}
-
-	// 5.A program to check if a binary tree is BST or not
-
-	// 6.Find k-th smallest element in BST (Order Statistics in BST)
-	public int kthlargest(int i) {
-		ArrayList<Integer> a = new ArrayList<>();
-		kthlargest(root, a);
-		int s = a.size();
-		return a.get(s - i);
+	public void InOrder() {
+		InOrder(this.root);
 
 	}
 
-	// static ArrayList<Integer> al;
-
-	public void kthlargest(Node node, ArrayList<Integer> a) {
+	private void InOrder(Node node) {
 		if (node == null) {
 			return;
 		}
-
-		kthlargest(node.left, a);
-
-		System.out.print(node.data + " ");
-		a.add(node.data);
-
-		kthlargest(node.right, a);
+		InOrder(node.left);
+		System.out.println(node.data);
+		InOrder(node.right);
 	}
 
-	// 7.Check if each internal node of a BST has exactly one child
-	// Given Preorder traversal of a BST, check if each non-leaf node has only
-	// one child.Assume that the BST contains unique entries.
-	// 8.Check for Identical BSTs without building the trees
-	// 9.K’th Largest Element in BST when modification to BST is not allowed
-	// 10.K’th Largest element in BST using constant extra space
-	// 11.Second largest element in BST
-	// 12.K’th smallest element in BST using O(1) Extra Space
-	// 13.Check if given sorted sub-sequence exists in binary search tree
-	public boolean SubSeqExist(int[] arr) {
-		int i = 0;
-        int size=arr.length;
-		SubSeqExist(root, arr, i);
-		return size == i;
+	/////////
+	// Level Order Traversal of a binary tree
+	public void levelOrder() {
+		levelOrder(this.root);
 	}
 
-	public void SubSeqExist(Node node, int[] arr, int i) {
+	private static void levelOrder(Node node) {
 		if (node == null) {
 			return;
 		}
-		SubSeqExist(node.left, arr, i);
-		if (node.data == arr[i]) {
-			i++;
-		}
-		SubSeqExist(node.right, arr, i);
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(node);
 
-	}
-	// 14.Simple Recursive solution to check whether BST contains dead end
-	//Here Dead End means, we are not able to insert any integer element after that node.
-	//15.Check if an array represents Inorder of Binary Search tree or not
-	
-	
-	//MISC.
-	//1.Sorted order printing of a given array that represents a BST
-	public void sort(int[]arr,int s,int e){
-		if(s>e){
-			return;
+		while (true) {
+			int count = q.size();
+			if (count == 0) {
+				break;
+			}
+			while (count > 0) {
+				Node poll = q.poll();
+				System.out.print(poll.data + " ");
+				// System.out.println(poll);
+				if (poll.left != null)
+					q.add(poll.left);
+				if (poll.right != null)
+					q.add(poll.right);
+				count--;
+			}
+			System.out.println();
 		}
-		sort(arr, s*2+1, e);
-		System.out.println(arr[s]);
-		sort(arr, s*2+2, e);
-	}
-	//2.Two nodes of a BST are swapped, correct the BST
-	//3.Floor and Ceil from a BST
-	public void Floor(Node node){
-		
-	}
 	}
 
+	// Lowest Common Ancestor in a Binary Search Tree.
+	public int LCA(int node1, int node2) {
+		return LCA(this.root, node1, node2).data;
+	}
+
+	private Node LCA(Node node, int node1, int node2) {
+		if (node == null) {
+			return null;
+		}
+		if (node.data > node1 && node.data > node2) {
+			return LCA(node.left, node1, node2);
+		}
+		if (node.data < node1 && node.data < node2) {
+			return LCA(node.right, node1, node2);
+		}
+
+		else
+			return node;
+	}
+
+	// Find k-th smallest element in BST
+	
+
+}
